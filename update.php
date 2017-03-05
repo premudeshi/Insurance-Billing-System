b3c758a6cd9dbc3c5966129a182541273c64d0d0
Prem Udeshi <premudeshi99@gmail.com>
2017-03-05 21:50:17 +0300

    2nd Commit to the Updation
    Working on the Updation. Now all are working apart from the final stage
    where the error of my SQL syntax comes!

--- update.php
+++ update.php
@@ -1,14 +1,42 @@
-<?php  
-mysql_connect('localhost', 'root', "");
-mysql_select_db('invoices');
+<!DOCTYPE HTML>
+<title>Home</title>
+<html>  
+<h2>Aspire Porforma Management System</h2>
+<h3>Update a Claim</h3>
+<body>
+<a href="create.php">Create a Claim</a> - 
+<a href="Update.php">Update Information</a> - 
+<a href="generatep.php">Generate a Performa</a> - 
+<a href="generatei.php">Generate an Invoice</a>  
 
-$sql = "SELECT loss , insured FROM claim";
-$result = mysql_query($sql);
+</body>
 
-echo "<select name='username'>";
-while ($row = mysql_fetch_array($result)) {
-    echo "<option value='" . $row['loss'] ."'>" . $row['loss'] ."</option>";
-}
-echo "</select>";
+<form action="updateclaim.php" method="post">
+    <p>
+        <label for="claimnumber">Claim Number (Extract from Google Sheet):</label>
+        <input type="text" name="claimnumber" id="claimnumber">
+    </p>
 
-?>
+    <p>
+        <label for="updateselection" id="updateselection">What to Update:</label>
+        <select name="updateselection">
+        <option value="insured">Insured</option>
+        <option value="insurance_company">Insurance Company</option>
+        <option value="kind_attn">Kind Attn</option>
+        <option value="our_ref">Refrance</option>
+        <option value="risk_note">Risk Note</option>
+        <option value="location_loss">Location of Loss</option>
+        <option value="loss">Loss</option>
+        <option value="broker">Broker</option>
+        </select>
+    </p>
+
+    <p>
+        <label for="updation">Update To:</label>
+        <input type="text" name="updation" id="updation" required>
+    </p>
+
+    <input type="submit" value="Submit">
+</form>
+</html>
+
---
+++ updateclaim.php
@@ -0,0 +1,28 @@
+
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "invoices");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$number = mysqli_real_escape_string($link, $_REQUEST['claimnumber']);
+$update = mysqli_real_escape_string($link, $_REQUEST['updateselection']);
+$updation = mysqli_real_escape_string($link, $_REQUEST['updation']);
+
+ 
+// attempt insert query execution
+$sql = "UPDATE `claim` SET $update = $updation WHERE `claim`.`claimnumber` = $number ";
+if(mysqli_query($link, $sql)){
+    echo "Claim added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// close connection
+mysqli_close($link);
+?>
