6115032207a4460ca53479f3b203991cb8dd91d6
Prem Udeshi <premudeshi99@gmail.com>
2017-03-06 18:21:45 +0300

    Commit 6
    View Feature Working, After tweaking most of the code, the viewing of
    claims is now working!

--- view.php
+++ view.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 	<title>View All Claims!</title>
+	<style>
+table, th, td {
+     border: 1px solid black;
+}
+</style>
 </head>
 <h2>View all Claims</h2>
 <h3>Search:</h3>
@@ -34,5 +39,32 @@
 <?php
 
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "invoices";
 
-?>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "SELECT * FROM claim";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     echo "<table><tr><th>ID</th><th>Name</th><th>Insurance Company</th><th>Kind Attn</th><th>Our Ref</th><th>Risk Note</th> <th>Location of Loss</th> <th>Loss</th><th>Date of Loss</th> <th>Broker</th> </tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["insured"]. "</td>  <td>" . $row["insurance_company"]. "</td>  <td>" . $row["kind_attn"]. "</td>  <td>" . $row["our_ref"]. "</td>  <td>" . $row["risk_note"]. "</td>  <td>" . $row["location_loss"]. "</td>  <td>" . $row["loss"]. "</td> <td>" . $row["date"]. "</td> <td>" . $row["broker"]. "</td> </tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+
+$conn->close();
+?>  
